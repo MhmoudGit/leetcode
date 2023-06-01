2304f2581c0f2bb11535dea28a3a141d8a2e4053
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2023-06-02 00:10:27 +0300

    main

---
+++ stock2.py
@@ -0,0 +1,11 @@
+def max_profit(prices: list[int]) -> int:
+    max_profit = 0
+
+    for i in range(1, len(prices)):
+        if prices[i] > prices[i - 1]:
+            max_profit += prices[i] - prices[i - 1]
+
+    return max_profit
+
+
+print(max_profit(prices=[7, 1, 5, 3, 6, 4]))
