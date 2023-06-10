f1fdfb1ee3e44237ac7fc1a1570e2b79b1293689
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2023-06-10 11:02:07 +0300

    j

--- jumpgame.py
+++ jumpgame.py
@@ -1,14 +1,22 @@
 def can_jump(nums: list[int]) -> bool:
-    target = nums[-1]
-    current = nums[0]
-    new = nums[current:]
-    while len(new) != 0:
-        current = new[0]
-        if current == target:
-            return True
-        else:
-            new[current:]
+    max_reach = 0  # Maximum index that can be reached
+    n = len(nums)
+    
+    for i in range(n):
+        # If the current index is beyond the maximum reach, return False
+        if i > max_reach:
             return False
+        
+        # Update the maximum reach if the current index + jump length is greater
+        if i + nums[i] > max_reach:
+            max_reach = i + nums[i]
+        
+        # If the maximum reach exceeds or equals the last index, return True
+        if max_reach >= n - 1:
+            return True
+    
+    return False  # Cannot reach the last index
+    
 
 
 print(can_jump(nums=[3, 2, 1, 1, 4]))
